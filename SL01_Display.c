bc08ed10520e89db3995d561efffde5eee24d302
VuVanHuy <huyvv10@fpt.edu.vn>
2026-09-08 12:11:08 +0700

    Initial commit

---
+++ SL01_Display.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(){
+	printf("I'm Huy. I'm an \"FPT student\"!");
+	printf("\nC:\\PRF192\\Workshop\\WS1.c");
+	return 0;
+}
---
+++ SL01_Helloworld.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+	//display to the screen
+/*	
+	printf("Hello world.");
+	printf("I am Huy.");
+
+	printf("\n");
+	printf("\n");
+*/
+	printf("Hello world.\nI am Huy.");
+
+	return 0;
+}
